0f1f6d6478da5c18ca88860bd459667a2df1ac0a
Devansh Rajoria <devanshrajoria@gmail.com>
2017-07-08 12:24:05 -0700

    Add files via upload

---
+++ Receiver.ino
@@ -0,0 +1,104 @@
+#include <VirtualWire.h>
+
+int leftmotorforward =27;
+int leftmotorbackward=26;
+int rightmotorforward=28;
+int rightmotorbackward=29;
+void setup()
+{
+Serial.begin(9600); // Debugging only
+Serial.println("setup");
+pinMode(13,OUTPUT);
+pinMode(leftmotorforward,OUTPUT);
+pinMode(leftmotorbackward,OUTPUT);
+pinMode(rightmotorforward,OUTPUT);
+pinMode(rightmotorbackward,OUTPUT);
+vw_set_rx_pin(2);
+// Initialise the IO and ISR
+vw_setup(2000); // Bits per sec
+vw_rx_start(); // Start the receiver PLL running
+}
+void loop()
+{
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buf, &buflen)) // Non-blocking
+    {
+  int i;
+
+        digitalWrite(13, true); // Flash a light to show received good message
+  for (i = 0; i < buflen; i++)
+  {
+   Serial.print(buf[i]);//print received command
+        if(buf[i] == '1')//if button 1 is pressed.... i.e.forward buton
+      {
+        forward();//go forward
+        Serial.println(" = forward");
+      }
+        if(buf[i] == '2'){
+          backward();
+          Serial.println(" = backward");
+        }
+        if(buf[i] == '3'){
+          moveLeft();
+          Serial.println(" = move Left");
+        }
+        if(buf[i] == '4'){
+          moveRight();
+          Serial.println(" = moveRight");
+        }
+  }
+  Serial.println("");
+        digitalWrite(13, false);
+
+   }//close if
+}//close loop
+void forward()
+{
+  digitalWrite(leftmotorforward,1);
+  digitalWrite(leftmotorbackward,0);
+  digitalWrite(rightmotorforward,1);
+  digitalWrite(rightmotorbackward,0);
+  delay(500);
+ digitalWrite(leftmotorforward,0);
+  digitalWrite(leftmotorbackward,0);
+  digitalWrite(rightmotorforward,0);
+  digitalWrite(rightmotorbackward,0); 
+}
+void backward(){
+  digitalWrite(leftmotorforward,0);
+  digitalWrite(leftmotorbackward,1);
+  digitalWrite(rightmotorforward,0);
+  digitalWrite(rightmotorbackward,1);
+   delay(500);
+ digitalWrite(leftmotorforward,0);
+  digitalWrite(leftmotorbackward,0);
+  digitalWrite(rightmotorforward,0);
+  digitalWrite(rightmotorbackward,0);
+} 
+
+void moveLeft()
+{
+  digitalWrite(leftmotorforward,0);
+  digitalWrite(leftmotorbackward,1);
+  digitalWrite(rightmotorforward,1);
+  digitalWrite(rightmotorbackward,0);
+   delay(500);
+ digitalWrite(leftmotorforward,0);
+  digitalWrite(leftmotorbackward,0);
+  digitalWrite(rightmotorforward,0);
+  digitalWrite(rightmotorbackward,0);
+}
+void moveRight()
+{
+  digitalWrite(leftmotorforward,1);
+  digitalWrite(leftmotorbackward,0);
+  digitalWrite(rightmotorforward,0);
+  digitalWrite(rightmotorbackward,1);
+   delay(500);
+ digitalWrite(leftmotorforward,0);
+  digitalWrite(leftmotorbackward,0);
+  digitalWrite(rightmotorforward,0);
+  digitalWrite(rightmotorbackward,0);
+}
---
+++ Transmitter.ino
@@ -0,0 +1,69 @@
+#include<VirtualWire.h>
+
+int forward = 4;
+int backward = 5;
+int moveLeft = 6;
+int moveRight = 7;
+
+void setup()
+{
+    Serial.begin(9600);    // Debugging only
+    Serial.println("setup");
+    // Initialise the IO and ISR
+    vw_setup(2000);  // Bits per sec
+    vw_set_tx_pin(3); //Transmitter Data Pin to Digital Pin 3
+    pinMode(forward,INPUT);
+    digitalWrite(forward,LOW);
+    pinMode(backward,INPUT);
+    digitalWrite(backward,LOW);
+    pinMode(moveLeft,INPUT);
+    digitalWrite(moveLeft,LOW);
+    pinMode(moveRight,INPUT);
+    digitalWrite(moveRight,LOW);
+
+}//close setup
+
+void loop()
+{
+  char *msg2;
+  
+  if(digitalRead(forward) == HIGH)//if the forward button is pressed
+  {
+    Serial.println("aage badh");    
+    char *msg2 = "1";//send 1 to the receiver
+    digitalWrite(13, true); // Flash a light to show transmitting
+    vw_send((uint8_t *)msg2, strlen(msg2));//send the byte to the receiver
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, false);
+  }
+  
+  if(digitalRead(backward) == HIGH)//if the forward button is pressed
+  { 
+    Serial.println("peeche ho le");    
+    char *msg2 = "2";//send 1 to the receiver
+    digitalWrite(13, true); // Flash a light to show transmitting
+    vw_send((uint8_t *)msg2, strlen(msg2));//send the byte to the receiver
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, false);
+  }
+
+  if(digitalRead(moveLeft) == HIGH)//if the forward button is pressed
+  { 
+    Serial.println("ghoom left");    
+    char *msg2 = "3";//send 1 to the receiver
+    digitalWrite(13, true); // Flash a light to show transmitting
+    vw_send((uint8_t *)msg2, strlen(msg2));//send the byte to the receiver
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, false);
+  }
+  
+    if(digitalRead(moveRight) == HIGH)//if the forward button is pressed
+  { 
+    Serial.println("ghoom right");    
+    char *msg2 = "4";//send 1 to the receiver
+    digitalWrite(13, true); // Flash a light to show transmitting
+    vw_send((uint8_t *)msg2, strlen(msg2));//send the byte to the receiver
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, false);
+  }
+}
